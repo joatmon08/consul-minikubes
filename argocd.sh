28689dc316041f33907058fe83eb85c164b76898
Rosemary Wang <915624+joatmon08@users.noreply.github.com>
2023-09-21 10:16:13 -0400

    Wait longer for host volumes to create

--- argocd.sh
+++ argocd.sh
@@ -22,6 +22,7 @@ install_consul () {
 fix_volumes () {
     for i in {1..20}; do kubectl --context ${1} get ns consul && break || sleep 15; done
     for i in {1..5}; do kubectl --context ${1} -n consul get pods consul-server-0 consul-server-1 consul-server-2 && break || sleep 15; done
+    sleep 30
     for i in {1..5}; do minikube -p ${1} -n ${1} ssh "sudo chmod -R 777 /tmp/hostpath-provisioner/consul" && break || sleep 15; done
     for i in {1..5}; do minikube -p ${1} -n ${1}-m02 ssh "sudo chmod -R 777 /tmp/hostpath-provisioner/consul" && break || sleep 15; done
     for i in {1..5}; do minikube -p ${1} -n ${1}-m03 ssh "sudo chmod -R 777 /tmp/hostpath-provisioner/consul" && break || sleep 15; done
